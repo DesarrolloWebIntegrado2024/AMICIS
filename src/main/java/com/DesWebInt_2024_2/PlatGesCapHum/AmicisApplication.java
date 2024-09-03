25b909a8571255b733ec0ec659170bd49c18a27c
Rodrigo <eldodi29@gmail.com>
2024-09-03 12:56:30 -0500

    spring_integration

---
+++ src/main/java/com/DesWebInt_2024_2/PlatGesCapHum/AmicisApplication.java
@@ -0,0 +1,13 @@
+package com.DesWebInt_2024_2.PlatGesCapHum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmicisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmicisApplication.class, args);
+	}
+
+}
